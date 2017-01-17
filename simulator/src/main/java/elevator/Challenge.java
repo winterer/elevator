19c6f54d63454b8b128dc54c5ddfefe041be6ff6
winterer <mario.winterer+github@gmail.com>
2017-01-18 00:37:28 +0100

    Added elevator.properties file for setting system properties

--- simulator/src/main/java/elevator/Challenge.java
+++ simulator/src/main/java/elevator/Challenge.java
@@ -5,19 +5,32 @@
 
 package elevator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 class Challenge {
+    private static Logger logger = LoggerFactory.getLogger(Challenge.class);
+
     static int logging = 0;
 
     Challenge() {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new Console();
-            }
-        });
+        try(InputStream inputStream = new FileInputStream("bin/elevator.properties")) {
+            System.getProperties().load(inputStream);
+        } catch (FileNotFoundException ex) {
+            logger.info("Properties file elevator.properties not found. Using default settings.");
+        } catch (IOException ex) {
+            logger.error("Error reading elevator.properties file", ex);
+        }
+
+        SwingUtilities.invokeLater(Console::new);
     }
 }
